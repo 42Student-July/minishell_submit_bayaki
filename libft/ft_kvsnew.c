422ae85ee6230e22f02ba9a0a850fd5ca0ba2336
bayamasa <mapi0213mgt@gmail.com>
2022-03-16 08:33:10 +0900

    MOD: x系関数に変更

--- libft/ft_kvsnew.c
+++ libft/ft_kvsnew.c
@@ -4,15 +4,15 @@ void	*ft_kvsnew(char *key, char *value)
 {
 	t_kvs	*kvs;
 
-	kvs = (t_kvs *)malloc(sizeof(t_kvs));
+	kvs = (t_kvs *)ft_xmalloc(sizeof(t_kvs));
 	if (kvs == NULL)
 		return (NULL);
-	kvs->key = ft_strdup(key);
+	kvs->key = ft_xstrdup(key);
 	if (kvs->key == NULL)
 		return (NULL);
 	if (value != NULL)
 	{
-		kvs->value = ft_strdup(value);
+		kvs->value = ft_xstrdup(value);
 		if (kvs->value == NULL)
 		{
 			free(kvs->key);
