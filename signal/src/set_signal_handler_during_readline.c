c585a421514e7da68b29fa3c1f54d2baf42985d5
akito <akito@student.42tokyo.jp>
2022-03-16 15:55:32 +0900

    norm signal

--- signal/src/set_signal_handler_during_readline.c
+++ signal/src/set_signal_handler_during_readline.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_signal_handler_during_readline.c               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akito <akito@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/16 15:54:46 by akito             #+#    #+#             */
+/*   Updated: 2022/03/16 15:54:47 by akito            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "sigaction.h"
 
 /* SIGINT */
+// print_exit_status_in_signal();
+// rl_replace_line("", 0);: clear buffer
+
 void	handle_sigint_during_readline(int sig)
 {
 	(void)sig;
 	g_exit_status = 1;
 	printf("\n");
-	// print_exit_status_in_signal();
-	rl_replace_line("", 0); // プロンプトのバッファをクリア
-	rl_on_new_line();       // プロンプトを次の行に移動したいことを伝える？
-	rl_redisplay();         // プロンプトを再描画
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 static void	set_sigint_handler_during_readline(void)
@@ -23,7 +36,7 @@ static void	set_sigint_handler_during_readline(void)
 	sigaction(SIGINT, &act, NULL);
 }
 
-static void set_sigquit_handler_during_readline(void)
+static void	set_sigquit_handler_during_readline(void)
 {
 	signal(SIGQUIT, SIG_IGN);
 }
