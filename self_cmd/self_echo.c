c1c32a4dfef7d3041e5ec35ff6e41f71f8f25104
Akihide Ito <39990441+AkkyOrz@users.noreply.github.com>
2022-03-18 15:47:15 +0900

    fix: echo -n で -n も表示されていたのを修正 (#40)

--- self_cmd/self_echo.c
+++ self_cmd/self_echo.c
@@ -3,42 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   self_echo.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: akito <akito@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:50:54 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/03/16 11:18:33 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/03/18 15:42:57 by akito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "self_cmd.h"
+#include <stdbool.h>
 
-void	check_option(t_list *args, bool *display_return, char *str)
+bool	is_n_option(char *str)
+{
+	int	i;
+
+	if (str == NULL || str[0] != '-')
+		return (false);
+	i = 1;
+	while (str[i])
+	{
+		if (ft_strchr("n", str[i]) == NULL)
+			return (false);
+		i++;
+	}
+	if (str[1] == '\0' || str[i] != '\0')
+		return (false);
+	return (true);
+}
+
+bool	check_n_option(t_list *args)
 {
 	size_t	i;
+	bool	display_return;
+	char	*str;
 
+	display_return = false;
 	while (args != NULL)
 	{
 		str = args->content;
-		if (str == NULL || str[0] != '-')
-			break ;
-		i = 1;
-		while (str[i])
-		{
-			if (ft_strchr("n", str[i]) == NULL)
-				break ;
-			i++;
-		}
-		if (str[1] == '\0' || str[i] != '\0')
+		if (!is_n_option(str))
 			break ;
 		i = 1;
 		while (str[i] != '\0')
 		{
 			if (str[i] == 'n')
-				*display_return = true;
+				display_return = true;
 			i++;
 		}
 		args = args->next;
 	}
+	return (display_return);
 }
 
 int	exec_self_echo(t_cmd *cmd, t_exec_attr *ea)
@@ -50,8 +64,9 @@ int	exec_self_echo(t_cmd *cmd, t_exec_attr *ea)
 	str = NULL;
 	args = cmd->args->next;
 	(void)ea;
-	display_return = false;
-	check_option(args, &display_return, str);
+	display_return = check_n_option(args);
+	while (args != NULL && is_n_option(args->content))
+		args = args->next;
 	while (args != NULL)
 	{
 		str = args->content;
